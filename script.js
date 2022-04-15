81259ac0ea3d803ee37fe2e543dfc5f2c48cf150
Aravind Krishna <akaravindk59@gmail.com>
2022-04-16 05:20:30 +0530

    second commit

--- script.js
+++ script.js
@@ -2,32 +2,35 @@ let formSubmit = document.getElementById('searchForm')
 formSubmit.addEventListener('submit', onFormSubmit)
 
 function onFormSubmit(e) {
-    e.preventDefault()
-    let nameString = document.getElementById('searchName').value
+  e.preventDefault()
+  let nameString = document.getElementById('searchName').value
+  getData(nameString)
+}
 
-    function printData(data) {
-        let results = document.getElementById('results')
-        results.innerHTML = `   
+function getData(nameString) {
+  fetch(`https://api.genderize.io?name=${nameString}`)
+    .then((response) => response.json())
+    .then((data) => printData(data))
+}
+
+function printData(data) {
+  let results = document.getElementById('results')
+  results.innerHTML = `   
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">${data.name}</h5>
                 <h6 class="card-subtitle mb-2">Possible Gender</h6>
                 <p class="card-text">Gender: ${data.gender}</p>
-                <p class="card-text">Probability: ${(data.probability * 100).toFixed(2)}%</p>
+                <p class="card-text">Probability: ${(
+                  data.probability * 100
+                ).toFixed(2)}%</p>
                 
             </div>
         </div>
         `
-    }
-
-    fetch(`https://api.genderize.io?name=${nameString}`)
-        .then(response => response.json())
-        .then(data => printData(data))
 }
 
 function clear1() {
-    let result = document.getElementById('results')
-    result.innerHTML = " "
-
+  let result = document.getElementById('results')
+  result.innerHTML = ' '
 }
-
