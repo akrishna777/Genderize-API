b27a361e6c0bb6eb72d34d92044decdc83242a19
Aravind Krishna <akaravindk59@gmail.com>
2022-04-16 01:20:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let formSubmit = document.getElementById('searchForm')
+formSubmit.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    let nameString = document.getElementById('searchName').value
+
+    function printData(data) {
+        let results = document.getElementById('results')
+        results.innerHTML = `   
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <h6 class="card-subtitle mb-2">Possible Gender</h6>
+                <p class="card-text">Gender: ${data.gender}</p>
+                <p class="card-text">Probability: ${(data.probability * 100).toFixed(2)}%</p>
+                
+            </div>
+        </div>
+        `
+    }
+
+    fetch(`https://api.genderize.io?name=${nameString}`)
+        .then(response => response.json())
+        .then(data => printData(data))
+}
+
+function clear1() {
+    let result = document.getElementById('results')
+    result.innerHTML = " "
+
+}
+
